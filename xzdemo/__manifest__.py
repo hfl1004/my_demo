0ba7dd7bab5f50662100fa4817164b08fff63f16
hfl1004 <44161676@qq.com>
2018-12-07 10:56:53 +0000

    day1

---
+++ xzdemo/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'XZPSCloud课程管理',
+    'version': '12.0.1.0',
+    'summary': 'XZPSCloud课程管理',
+    'author': "www.mypscloud.com",
+    'website': 'https://www.mypscloud.com/',
+    'category': 'Tools',
+    'depends': ['base'], 
+    'demo': [
+        'demo/pscloud_demo.xml',
+    ],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'auto_install': False,
+    'application': True,
+}
