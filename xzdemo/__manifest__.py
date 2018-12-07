1675146ae45c0cb07bbe5c2f9e89797dd03c20a0
hfl1004 <45680164+hfl1004@users.noreply.github.com>
2018-12-07 23:58:01 +0800

    Update __manifest__.py

--- xzdemo/__manifest__.py
+++ xzdemo/__manifest__.py
@@ -7,7 +7,12 @@
     'website': 'https://www.mypscloud.com/',
     'category': 'Tools',
     'depends': ['base'],
-
+    'data': [
+        'views/res_partner_views.xml',
+        'views/lesson_views.xml',
+        'views/subject_views.xml',
+        'views/views.xml',
+    ],
     'qweb': [],
     'js': [],
     'css': [],
