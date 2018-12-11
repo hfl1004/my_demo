3694fc5705ff07be46e7d5ba9c540bfdfb692b6d
hfl1004 <44161676@qq.com>
2018-12-11 08:34:54 +0000

    hflday1

--- xzdemo/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ xzdemo/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,6 +2,6 @@
 
 from . import subject
 #from . import lesson
-#from . import res_partner
+from . import res_partner
 
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
--- xzdemo/models/.ipynb_checkpoints/subject-checkpoint.py
+++ xzdemo/models/.ipynb_checkpoints/subject-checkpoint.py
@@ -3,13 +3,12 @@
 from odoo import api, fields, models
 
 class Subject(models.Model):
-    _name = 'pscloud.xzdemo.subject'
+    _name = 'my_demo.xzdemo.subject'
     _description = "科目"
 
     name = fields.Char(string='科目名称')
     person_id = fields.Many2one('res.partner', string='负责人')
     lessin_ids=fields.Char(string='课程')
-#    lesson_ids = fields.One2many('pscloud.xzdemo.lesson', 'subject_id', string='课程')
     desc = fields.Text(string='描述')
 
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
